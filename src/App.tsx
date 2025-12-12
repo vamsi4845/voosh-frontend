f0165b8154cde3c7bc4942224ac742efb0befe40
vamsisai <vamsisai.karumuri@gmail.com>
2025-12-12 21:55:26 +0530

    chore: init Vite project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatScreen from './components/ChatScreen';
+
+function App(): JSX.Element {
+  return (
+    <div className="app">
+      <ChatScreen />
+    </div>
+  );
+}
+
+export default App;
+
